c3191b7c8645bf9a85a33603674c0c7553217cf7
wwwYoungmin <bowwow515@hanyang.ac.kr>
2022-05-19 21:04:15 +0900

    Add: Youngmin blog post feature

--- wwwyoungmin/script.js
+++ wwwyoungmin/script.js
@@ -1,5 +1,6 @@
 import data from "./data.js";
 
+//main card-list
 let i = 0;
 for (i = 0; i < data.length; i++) {
   $("#card-list").append(
@@ -17,3 +18,39 @@ for (i = 0; i < data.length; i++) {
     `
   );
 }
+
+//modal date
+var today = new Date();
+$("#post-date").append(`
+  <time datetime="${today}">${today}</time>
+`);
+
+//modal
+//data save to localStorage (session storage에서 브라우저 닫으면 삭제되는게 싫어서 local storage로 함.)
+var modalBg = document.getElementById("modal-background");
+var postBtn = document.getElementById("postBtn");
+var saveBtn = document.getElementById("saveBtn");
+var postTitleTest = document.getElementById("post-title-read-test");
+var postContentTest = document.getElementById("post-content-read-test");
+
+postBtn.onclick = function () {
+  modalBg.style.display = "block";
+};
+saveBtn.onclick = function () {
+  modalBg.style.display = "none";
+  localStorage.setItem(
+    "title",
+    document.getElementsByName("post-title")[0].value
+  );
+  localStorage.setItem(
+    "content",
+    document.getElementsByName("post-content")[0].value
+  );
+  postTitleTest.innerHTML += localStorage.getItem("title");
+  postContentTest.innerHTML += localStorage.getItem("content");
+};
+modalBg.onclick = function (event) {
+  if (event.target == modalBg) {
+    modalBg.style.display = "none";
+  }
+};
